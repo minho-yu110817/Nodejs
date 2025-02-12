8b640aacb5e4c1f71e29f1fae9dcc02c25164f99
minho-yu110817 <meyh110817@naver.com>
2025-02-12 09:30:19 +0900

    server.js 업데이트

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-// 정적 파일 제공 (public 폴더)
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
